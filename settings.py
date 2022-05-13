f744397f1f447a32894a710d31128050fd1e3260
German Pinedo <germanpinedo13@gmail.com>
2022-05-13 22:06:57 +0200

    initial

--- settings.py
+++ settings.py
@@ -2,18 +2,18 @@ import os
 """ Dataset directories """
 DATASET_DIR = 'dataset/'
 
-TRAIN_IMAGES = DATASET_DIR + 'new_128_3C/train_images/'
-TRAIN_MASKS = DATASET_DIR + 'new_128_3C/train_masks/'
-VAL_IMAGES = DATASET_DIR + 'new_128_3C/val_images/'
-VAL_MASKS = DATASET_DIR + 'new_128_3C/val_masks/'
+TRAIN_IMAGES = DATASET_DIR + 'train_images/'
+TRAIN_MASKS = DATASET_DIR + 'ntrain_masks/'
+VAL_IMAGES = DATASET_DIR + 'val_images/'
+VAL_MASKS = DATASET_DIR + 'val_masks/'
 
 """ HYPER-PARAMETERS """
 LOSS_FN = 'dice loss'
-EPOCHS = 800
-BATCH_SIZE = 128
+EPOCHS = 100
+BATCH_SIZE = 32
 LEARNING_RATE = 0.001
 BETA1 = 0.9
-BETA2 = 0.95
+BETA2 = 0.999
 WEIGHT_DECAY = 0.0001
 CLASS_WEIGHTS = [1, 1, 1, 1]
 SCHEDULER = 'step' #step, cosine
@@ -27,6 +27,7 @@ IMAGE_SIZE = 128
 CLASSES = 4
 NUM_WORKERS = os.cpu_count()
 
+""" swin-transformer settings """
 embed_dim = 24
 depths = [2, 2, 2, 2]
 num_heads = [3, 6, 12, 24]
